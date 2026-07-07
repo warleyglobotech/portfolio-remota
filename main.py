762ab9c7be0ca28ebd115344b24d6e1f14fb3ec1
Warley <warley.santos.oliveira.mg.gov@gmail.com>
2026-07-07 07:46:34 -0300

    fix: adiciona liberacao de cors para rede

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # O asterisco significa "permitir de qualquer origem"
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 import logging
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
